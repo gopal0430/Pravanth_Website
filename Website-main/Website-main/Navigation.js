c51c303ab57434c2489bbaa30087827b305c2f51
gopal0430 <gopalakrishnan0430@gmail.com>
2024-08-26 09:09:10 +0530

    Initial Upload of project

---
+++ Website-main/Website-main/Navigation.js
@@ -0,0 +1,10 @@
+
+// Java script toggle menu 
+var navlinks = document.getElementById("navlinks");
+ 
+              function showMenu(){
+                  navlinks.style.right="0";
+              }
+              function hideMenu(){
+                  navlinks.style.right="-200px";
+              }
